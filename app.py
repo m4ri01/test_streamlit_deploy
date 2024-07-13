54ef0b7e6d53e2e4fc88896cab31073723dd8a8a
Melki Mario Gulo <elhackerz.9je.19@gmail.com>
2024-07-13 13:10:01 +0700

    add files

---
+++ app.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+st.title('First APPS!')
+st.subheader('I am creating my first app using Streamlit')
+
+st.image("https://petcostumecenter.com/wp-content/uploads/2020/05/straw-sombrero-pet-hat--scaled.jpg",caption='Sombrero Hat')
+
+st.write("look at this cute sombrero hat for pets!")
